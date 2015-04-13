5bb0b9783a17b4ec278358eef074012665ed9328
Artyom <yom@artyom.me>
2015-04-13 23:46:42 +0300

    Rewrite Lens.Micro.Mtl's docs a bit

--- microlens-mtl/src/Lens/Micro/Mtl.hs
+++ microlens-mtl/src/Lens/Micro/Mtl.hs
@@ -19,6 +19,19 @@ import Lens.Micro.Extras
 {- |
 'view' is a synonym for ('^.'), generalised for 'MonadReader' (since
 functions are instances of the 'MonadReader' class).
+
+>>> view _1 (1, 2)
+1
+
+It's often used when dealing with environment, for instance:
+
+@
+doSomething :: ('MonadReader' Config m) => m Int
+doSomething = do
+  thingy        <- 'view' setting1  -- same as “'asks' (^. setting1)”
+  anotherThingy <- 'view' setting2
+  ...
+@
 -}
 view :: MonadReader s m => Getting a s a -> m a
 view l = asks (getConst . l Const)
@@ -26,6 +39,10 @@ view l = asks (getConst . l Const)
 
 {- |
 'use' is 'view' which implicitly operates on the state.
+
+@
+'use' l = 'gets' ('view' l)
+@
 -}
 use :: MonadState s m => Getting a s a -> m a
 use l = gets (view l)
@@ -35,62 +52,67 @@ use l = gets (view l)
 infix  4 .=, %=
 infix  4 +=, -=, *=, //=
 
--- |
--- Assign value to the target. This is '.~' which works in 'State'.
---
--- @
--- l '.=' b = 'modify' (l '.~' b)
--- @
+{- |
+Assign value to the target. This is '.~' which works in 'State'.
+
+@
+l '.=' b = 'modify' (l '.~' b)
+@
+-}
 (.=) :: MonadState s m => ASetter s s a b -> b -> m ()
 l .= b = modify (l .~ b)
 {-# INLINE (.=) #-}
 
--- |
--- Apply a function to the target. This is '%~' which works in 'State'.
---
--- >>> execState (do _1 %= (+1); _2 %= reverse) (1,"hello")
--- (2,"olleh")
---
+{- |
+Apply a function to the target. This is '%~' which works in 'State'.
+
+>>> execState (do _1 %= (+1); _2 %= reverse) (1,"hello")
+(2,"olleh")
+-}
 (%=) :: (MonadState s m) => ASetter s s a b -> (a -> b) -> m ()
 l %= f = modify (l %~ f)
 {-# INLINE (%=) #-}
 
--- |
--- Add a number to the target.
---
--- @
--- l '+=' x = l '%=' (+x)
--- @
+{- |
+Add a number to the target.
+
+@
+l '+=' x = l '%=' (+x)
+@
+-}
 (+=) :: (MonadState s m, Num a) => ASetter s s a a -> a -> m ()
 l += b = modify (l +~ b)
 {-# INLINE (+=) #-}
 
--- |
--- Subtract a number from the target.
---
--- @
--- l '-=' x = l '%=' ('subtract' x)
--- @
+{- |
+Subtract a number from the target.
+
+@
+l '-=' x = l '%=' ('subtract' x)
+@
+-}
 (-=) :: (MonadState s m, Num a) => ASetter s s a a -> a -> m ()
 l -= b = modify (l -~ b)
 {-# INLINE (-=) #-}
 
--- |
--- Multiply the target by a number.
---
--- @
--- l '*=' x = l '%=' (*x)
--- @
+{- |
+Multiply the target by a number.
+
+@
+l '*=' x = l '%=' (*x)
+@
+-}
 (*=) :: (MonadState s m, Num a) => ASetter s s a a -> a -> m ()
 l *= b = modify (l *~ b)
 {-# INLINE (*=) #-}
 
--- |
--- Divide the target by a number.
---
--- @
--- l += x = l %= (+x)
--- @
+{- |
+Divide the target by a number.
+
+@
+l //= x = l %= (/x)
+@
+-}
 (//=) :: (MonadState s m, Fractional a) => ASetter s s a a -> a -> m ()
 l //= a = modify (l //~ a)
 {-# INLINE (//=) #-}
