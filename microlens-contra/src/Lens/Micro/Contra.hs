fc06c933c767c3c0be1ee83ce5f625580fecea66
Artyom <yom@artyom.me>
2015-12-29 20:01:36 +0300

    Add a new library: microlens-contra

---
+++ microlens-contra/src/Lens/Micro/Contra.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE
+TemplateHaskell,
+RankNTypes,
+FlexibleContexts
+  #-}
+
+
+module Lens.Micro.Contra
+(
+  -- * Getter
+  Getter,
+  fromSimpleGetter,
+
+  -- * Fold
+  Fold,
+  fromSimpleFold,
+)
+where
+
+
+import Lens.Micro
+import Lens.Micro.Extras (SimpleGetter, SimpleFold, view)
+
+import Data.Foldable (traverse_)
+import Data.Functor.Contravariant (phantom, Contravariant)
+
+
+type Getter s a =
+  forall f. (Contravariant f, Functor f) => (a -> f a) -> s -> f s
+
+fromSimpleGetter :: SimpleGetter s a -> Getter s a
+fromSimpleGetter g f = phantom . f . view g
+{-# INLINE fromSimpleGetter #-}
+
+type Fold s a =
+  forall f. (Contravariant f, Applicative f) => (a -> f a) -> s -> f s
+
+fromSimpleFold :: SimpleFold s a -> Fold s a
+fromSimpleFold g f = phantom . traverse_ f . toListOf g
+{-# INLINE fromSimpleFold #-}
