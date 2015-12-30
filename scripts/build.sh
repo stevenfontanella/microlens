2e5048ab51bdd70ac140016662dd82c9ff898ed7
Artyom <yom@artyom.me>
2015-12-30 21:28:44 +0300

    Actually fail the build if installing fails

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ev
+
+function buildlib {
+  cd $1
+  cabal install --only-dependencies --dry-run > plan.txt
+  if grep -Fq microlens plan.txt
+  then
+    cat plan.txt
+    echo "installing $lib led to downloading some microlens package!"
+    exit 1
+  fi
+  rm plan.txt
+  cabal install
+  cabal check
+  cabal sdist
+  cd ..
+}
+
+# We install packages in the order specified in install-order.txt;
+# if installing any package leads to *downloading* some microlens package,
+# it's a bug.
+while read lib; do buildlib $lib; done < install-order.txt
