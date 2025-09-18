a8d75ac12735e5d9962b51822dbcd0f2103267f3
Steven Fontanella <steven.fontanella@gmail.com>
2025-09-18 08:46:18 -0700

    Expose FieldN classes and instances from Lens.Micro.Internal to Lens.Micro
    Also add FieldN classes and instances up to Field9

--- microlens/src/Lens/Micro.hs
+++ microlens/src/Lens/Micro.hs
@@ -80,7 +80,8 @@ module Lens.Micro
   Lens, Lens',
   lens,
   at,
-  _1, _2, _3, _4, _5,
+  -- _1, _2, _3, _4, _5,
+  module Lens.Micro.FieldN,
 
   -- * Iso: a lens that only changes the representation
   -- $isos-note
@@ -115,7 +116,7 @@ module Lens.Micro
 )
 where
 
-
+import Lens.Micro.FieldN
 import Lens.Micro.Type
 import Lens.Micro.Internal
 
@@ -1623,3 +1624,215 @@ instance (Monad m) => Monad (StateT s m) where
 instance (Fail.MonadFail m) => Fail.MonadFail (StateT s m) where
     fail str = StateT $ \ _ -> Fail.fail str
 #endif
+
+
+-- class Field1 s t a b | s -> a, t -> b, s b -> t, t a -> s where
+--   {- |
+-- Gives access to the 1st field of a tuple (up to 5-tuples).
+
+-- Getting the 1st component:
+
+-- >>> (1,2,3,4,5) ^. _1
+-- 1
+
+-- Setting the 1st component:
+
+-- >>> (1,2,3) & _1 .~ 10
+-- (10,2,3)
+
+-- Note that this lens is lazy, and can set fields even of 'undefined':
+
+-- >>> set _1 10 undefined :: (Int, Int)
+-- (10,*** Exception: Prelude.undefined
+
+-- This is done to avoid violating a lens law stating that you can get back what you put:
+
+-- >>> view _1 . set _1 10 $ (undefined :: (Int, Int))
+-- 10
+
+-- The implementation (for 2-tuples) is:
+
+-- @
+-- '_1' f t = (,) '<$>' f    ('fst' t)
+--              '<*>' 'pure' ('snd' t)
+-- @
+
+-- or, alternatively,
+
+-- @
+-- '_1' f ~(a,b) = (\\a' -> (a',b)) '<$>' f a
+-- @
+
+-- (where @~@ means a <https://wiki.haskell.org/Lazy_pattern_match lazy pattern>).
+
+-- '_2', '_3', '_4', and '_5' are also available (see below).
+--   -}
+--   _1 :: Lens s t a b
+
+-- instance Field1 (a,b) (a',b) a a' where
+--   _1 k ~(a,b) = (\a' -> (a',b)) <$> k a
+--   {-# INLINE _1 #-}
+
+-- instance Field1 (a,b,c) (a',b,c) a a' where
+--   _1 k ~(a,b,c) = (\a' -> (a',b,c)) <$> k a
+--   {-# INLINE _1 #-}
+
+-- instance Field1 (a,b,c,d) (a',b,c,d) a a' where
+--   _1 k ~(a,b,c,d) = (\a' -> (a',b,c,d)) <$> k a
+--   {-# INLINE _1 #-}
+
+-- instance Field1 (a,b,c,d,e) (a',b,c,d,e) a a' where
+--   _1 k ~(a,b,c,d,e) = (\a' -> (a',b,c,d,e)) <$> k a
+--   {-# INLINE _1 #-}
+
+-- {-
+
+-- instance Field1 (a,b,c,d,e,f) (a',b,c,d,e,f) a a' where
+--   _1 k ~(a,b,c,d,e,f) = (\a' -> (a',b,c,d,e,f)) <$> k a
+--   {-# INLINE _1 #-}
+
+-- instance Field1 (a,b,c,d,e,f,g) (a',b,c,d,e,f,g) a a' where
+--   _1 k ~(a,b,c,d,e,f,g) = (\a' -> (a',b,c,d,e,f,g)) <$> k a
+--   {-# INLINE _1 #-}
+
+-- instance Field1 (a,b,c,d,e,f,g,h) (a',b,c,d,e,f,g,h) a a' where
+--   _1 k ~(a,b,c,d,e,f,g,h) = (\a' -> (a',b,c,d,e,f,g,h)) <$> k a
+--   {-# INLINE _1 #-}
+
+-- instance Field1 (a,b,c,d,e,f,g,h,i) (a',b,c,d,e,f,g,h,i) a a' where
+--   _1 k ~(a,b,c,d,e,f,g,h,i) = (\a' -> (a',b,c,d,e,f,g,h,i)) <$> k a
+--   {-# INLINE _1 #-}
+
+-- -}
+
+-- class Field2 s t a b | s -> a, t -> b, s b -> t, t a -> s where
+--   _2 :: Lens s t a b
+
+-- instance Field2 (a,b) (a,b') b b' where
+--   _2 k ~(a,b) = (\b' -> (a,b')) <$> k b
+--   {-# INLINE _2 #-}
+
+-- instance Field2 (a,b,c) (a,b',c) b b' where
+--   _2 k ~(a,b,c) = (\b' -> (a,b',c)) <$> k b
+--   {-# INLINE _2 #-}
+
+-- instance Field2 (a,b,c,d) (a,b',c,d) b b' where
+--   _2 k ~(a,b,c,d) = (\b' -> (a,b',c,d)) <$> k b
+--   {-# INLINE _2 #-}
+
+-- instance Field2 (a,b,c,d,e) (a,b',c,d,e) b b' where
+--   _2 k ~(a,b,c,d,e) = (\b' -> (a,b',c,d,e)) <$> k b
+--   {-# INLINE _2 #-}
+
+-- {-
+
+-- instance Field2 (a,b,c,d,e,f) (a,b',c,d,e,f) b b' where
+--   _2 k ~(a,b,c,d,e,f) = (\b' -> (a,b',c,d,e,f)) <$> k b
+--   {-# INLINE _2 #-}
+
+-- instance Field2 (a,b,c,d,e,f,g) (a,b',c,d,e,f,g) b b' where
+--   _2 k ~(a,b,c,d,e,f,g) = (\b' -> (a,b',c,d,e,f,g)) <$> k b
+--   {-# INLINE _2 #-}
+
+-- instance Field2 (a,b,c,d,e,f,g,h) (a,b',c,d,e,f,g,h) b b' where
+--   _2 k ~(a,b,c,d,e,f,g,h) = (\b' -> (a,b',c,d,e,f,g,h)) <$> k b
+--   {-# INLINE _2 #-}
+
+-- instance Field2 (a,b,c,d,e,f,g,h,i) (a,b',c,d,e,f,g,h,i) b b' where
+--   _2 k ~(a,b,c,d,e,f,g,h,i) = (\b' -> (a,b',c,d,e,f,g,h,i)) <$> k b
+--   {-# INLINE _2 #-}
+
+-- -}
+
+-- class Field3 s t a b | s -> a, t -> b, s b -> t, t a -> s where
+--   _3 :: Lens s t a b
+
+-- instance Field3 (a,b,c) (a,b,c') c c' where
+--   _3 k ~(a,b,c) = (\c' -> (a,b,c')) <$> k c
+--   {-# INLINE _3 #-}
+
+-- instance Field3 (a,b,c,d) (a,b,c',d) c c' where
+--   _3 k ~(a,b,c,d) = (\c' -> (a,b,c',d)) <$> k c
+--   {-# INLINE _3 #-}
+
+-- instance Field3 (a,b,c,d,e) (a,b,c',d,e) c c' where
+--   _3 k ~(a,b,c,d,e) = (\c' -> (a,b,c',d,e)) <$> k c
+--   {-# INLINE _3 #-}
+
+-- {-
+
+-- instance Field3 (a,b,c,d,e,f) (a,b,c',d,e,f) c c' where
+--   _3 k ~(a,b,c,d,e,f) = (\c' -> (a,b,c',d,e,f)) <$> k c
+--   {-# INLINE _3 #-}
+
+-- instance Field3 (a,b,c,d,e,f,g) (a,b,c',d,e,f,g) c c' where
+--   _3 k ~(a,b,c,d,e,f,g) = (\c' -> (a,b,c',d,e,f,g)) <$> k c
+--   {-# INLINE _3 #-}
+
+-- instance Field3 (a,b,c,d,e,f,g,h) (a,b,c',d,e,f,g,h) c c' where
+--   _3 k ~(a,b,c,d,e,f,g,h) = (\c' -> (a,b,c',d,e,f,g,h)) <$> k c
+--   {-# INLINE _3 #-}
+
+-- instance Field3 (a,b,c,d,e,f,g,h,i) (a,b,c',d,e,f,g,h,i) c c' where
+--   _3 k ~(a,b,c,d,e,f,g,h,i) = (\c' -> (a,b,c',d,e,f,g,h,i)) <$> k c
+--   {-# INLINE _3 #-}
+
+-- -}
+
+-- class Field4 s t a b | s -> a, t -> b, s b -> t, t a -> s where
+--   _4 :: Lens s t a b
+
+-- instance Field4 (a,b,c,d) (a,b,c,d') d d' where
+--   _4 k ~(a,b,c,d) = (\d' -> (a,b,c,d')) <$> k d
+--   {-# INLINE _4 #-}
+
+-- instance Field4 (a,b,c,d,e) (a,b,c,d',e) d d' where
+--   _4 k ~(a,b,c,d,e) = (\d' -> (a,b,c,d',e)) <$> k d
+--   {-# INLINE _4 #-}
+
+-- {-
+
+-- instance Field4 (a,b,c,d,e,f) (a,b,c,d',e,f) d d' where
+--   _4 k ~(a,b,c,d,e,f) = (\d' -> (a,b,c,d',e,f)) <$> k d
+--   {-# INLINE _4 #-}
+
+-- instance Field4 (a,b,c,d,e,f,g) (a,b,c,d',e,f,g) d d' where
+--   _4 k ~(a,b,c,d,e,f,g) = (\d' -> (a,b,c,d',e,f,g)) <$> k d
+--   {-# INLINE _4 #-}
+
+-- instance Field4 (a,b,c,d,e,f,g,h) (a,b,c,d',e,f,g,h) d d' where
+--   _4 k ~(a,b,c,d,e,f,g,h) = (\d' -> (a,b,c,d',e,f,g,h)) <$> k d
+--   {-# INLINE _4 #-}
+
+-- instance Field4 (a,b,c,d,e,f,g,h,i) (a,b,c,d',e,f,g,h,i) d d' where
+--   _4 k ~(a,b,c,d,e,f,g,h,i) = (\d' -> (a,b,c,d',e,f,g,h,i)) <$> k d
+--   {-# INLINE _4 #-}
+
+-- -}
+
+-- class Field5 s t a b | s -> a, t -> b, s b -> t, t a -> s where
+--   _5 :: Lens s t a b
+
+-- instance Field5 (a,b,c,d,e) (a,b,c,d,e') e e' where
+--   _5 k ~(a,b,c,d,e) = (\e' -> (a,b,c,d,e')) <$> k e
+--   {-# INLINE _5 #-}
+
+-- {-
+
+-- instance Field5 (a,b,c,d,e,f) (a,b,c,d,e',f) e e' where
+--   _5 k ~(a,b,c,d,e,f) = (\e' -> (a,b,c,d,e',f)) <$> k e
+--   {-# INLINE _5 #-}
+
+-- instance Field5 (a,b,c,d,e,f,g) (a,b,c,d,e',f,g) e e' where
+--   _5 k ~(a,b,c,d,e,f,g) = (\e' -> (a,b,c,d,e',f,g)) <$> k e
+--   {-# INLINE _5 #-}
+
+-- instance Field5 (a,b,c,d,e,f,g,h) (a,b,c,d,e',f,g,h) e e' where
+--   _5 k ~(a,b,c,d,e,f,g,h) = (\e' -> (a,b,c,d,e',f,g,h)) <$> k e
+--   {-# INLINE _5 #-}
+
+-- instance Field5 (a,b,c,d,e,f,g,h,i) (a,b,c,d,e',f,g,h,i) e e' where
+--   _5 k ~(a,b,c,d,e,f,g,h,i) = (\e' -> (a,b,c,d,e',f,g,h,i)) <$> k e
+--   {-# INLINE _5 #-}
+
+-- -}
